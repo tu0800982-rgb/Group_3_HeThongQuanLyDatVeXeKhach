256d966440469f494f58d50a919c409b4432fa4a
Chandongzzz <24107609@st.phenikaa-uni.edu.vn>
2026-06-27 02:06:30 +0000

    setup spring boot

---
+++ bus-ticket-management/bus-ticket-management/src/main/java/com/example/bus_ticket_management/BusTicketManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.bus_ticket_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketManagementApplication.class, args);
+	}
+
+}
---
+++ bus-ticket-management/bus-ticket-management/src/test/java/com/example/bus_ticket_management/BusTicketManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bus_ticket_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
