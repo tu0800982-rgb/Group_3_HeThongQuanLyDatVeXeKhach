02228889d7f11119586bd7181419aac31c9fa405
Chandongzzz <24107609@st.phenikaa-uni.edu.vn>
2026-06-28 21:29:22 +0700

    tao du an spring boot

---
+++ bus-ticket-booking/src/test/java/com/example/bus_ticket_booking/BusTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bus_ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
