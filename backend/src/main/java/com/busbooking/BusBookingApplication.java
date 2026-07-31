2e7a50cb3d44235e89c22bca2c63d25c80c26cec
tu0800982-rgb <tu0800982@gmail.com>
2026-07-31 23:52:40 +0700

    BusBookingApplication

---
+++ backend/src/main/java/com/busbooking/BusBookingApplication.java
@@ -0,0 +1,11 @@
+package com.busbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusBookingApplication.class, args);
+    }
+}
