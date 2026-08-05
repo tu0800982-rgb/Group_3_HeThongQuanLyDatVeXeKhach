a248aaf7e53ab444d93ed19a6eba3fd762bc1516
tu0800982-rgb <tu0800982@gmail.com>
2026-08-06 01:37:09 +0700

    fix

--- backend/src/main/java/com/busbooking/dto/TransactionHistoryDTO.java
+++ backend/src/main/java/com/busbooking/dto/TransactionHistoryDTO.java
@@ -1,11 +1,6 @@
 package com.busbooking.dto;
 
-<<<<<<< HEAD
-public class TransactionHistoryDTO {
-    
-=======
 import java.time.LocalDateTime;
 
 public record TransactionHistoryDTO(LocalDateTime time, String type, String description) {
->>>>>>> origin/main
 }
